9b70e82f0aa10ffa7945e404ac6c80b5d918182f
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-12 13:44:03 -0400

    Add sidebar items and add custom SCSS styling to sidebar

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,18 +1,23 @@
 import React from "react";
-// import { SideBarItem } from "./SideBarItem/SideBarItem";
+import { SidebarItem } from "./SidebarItem/SidebarItem";
 import { Menu } from "semantic-ui-react";
 import "./Sidebar.scss";
 
 export class Sidebar extends React.Component {
   render() {
     return (
-      <Menu
-        borderless
-        vertical
-        stackable
-        fixed="left"
-        className="side-nav"
-      ></Menu>
+      <Menu borderless vertical stackable fixed="left" className="side-nav">
+        <SidebarItem highlight={true} label="Home" icon="home" />
+        <SidebarItem label="Trending" icon="fire" />
+        <SidebarItem label="Followers" icon="spy" />
+        <SidebarItem label="History" icon="history" />
+        <SidebarItem label="Watch later" icon="clock" />
+        <SidebarItem label="Liked videos" icon="thumbs up" />
+        <SidebarItem label="Movies and Shows" icon="film" />
+        <SidebarItem label="Report history" icon="flag" />
+        <SidebarItem label="Help" icon="help circle" />
+        <SidebarItem label="Send feedback" icon="comment" />
+      </Menu>
     );
   }
 }
