9bc2ec3735f8d2adfbfe1a7a7d7bffbe80fb8d1e
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-12 13:59:45 -0400

    Create SidebarHeader component for a title for each sidebar section

--- src/containers/Sidebar/Sidebar.js
+++ src/containers/Sidebar/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { SidebarItem } from "./SidebarItem/SidebarItem";
-import { Menu } from "semantic-ui-react";
+import { Menu, Divider } from "semantic-ui-react";
 import "./Sidebar.scss";
+import { SidebarHeader } from "./SidebarHeader/SidebarHeader.js";
 
 export class Sidebar extends React.Component {
   render() {
@@ -10,13 +11,19 @@ export class Sidebar extends React.Component {
         <SidebarItem highlight={true} label="Home" icon="home" />
         <SidebarItem label="Trending" icon="fire" />
         <SidebarItem label="Followers" icon="spy" />
+        <Divider />
+        <SidebarHeader title="library" />
         <SidebarItem label="History" icon="history" />
         <SidebarItem label="Watch later" icon="clock" />
         <SidebarItem label="Liked videos" icon="thumbs up" />
+        <Divider />
+        <SidebarHeader title="movies and shows" />
         <SidebarItem label="Movies and Shows" icon="film" />
+        <Divider />
         <SidebarItem label="Report history" icon="flag" />
         <SidebarItem label="Help" icon="help circle" />
         <SidebarItem label="Send feedback" icon="comment" />
+        <Divider />
       </Menu>
     );
   }
