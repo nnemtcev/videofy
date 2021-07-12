3f7ffd3347f15162fd03b0cd9ef154983830ff15
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-12 13:00:10 -0400

    Create search bar and search button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { HeaderNav } from "./containers/HeaderNav/HeaderNav.js";
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <HeaderNav />
+      </div>
+    );
   }
 }
 
--- src/containers/HeaderNav/HeaderNav.js
+++ src/containers/HeaderNav/HeaderNav.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Image, Menu } from "semantic-ui-react";
+import { Image, Menu, Form, Input } from "semantic-ui-react";
 import "./HeaderNav.scss";
-import logo from "../../assets/images/logo.jpg";
+import logo from "../../assets/images/logo.jpeg";
 
 export class HeaderNav extends React.Component {
   render() {
@@ -10,7 +10,15 @@ export class HeaderNav extends React.Component {
         <Menu.Item header className="logo">
           <Image src={logo} size="tiny" />
         </Menu.Item>
-        <Menu.Menu className="nav-container"></Menu.Menu>
+        <Menu.Menu className="nav-container">
+          <Menu.Item className="search-input">
+            <Form>
+              <Form.Field>
+                <Input placeholder="Search" size="small" action="Go" />
+              </Form.Field>
+            </Form>
+          </Menu.Item>
+        </Menu.Menu>
       </Menu>
     );
   }
