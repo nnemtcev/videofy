1f0fa6ad6774d955b383a59eb893272f74bc6f9a
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-13 11:16:27 -0400

    Write snapshot tests for SidebarItem covering additional cases

--- src/containers/Sidebar/SidebarItem/__tests__/SidebarItem.unit.test.js
+++ src/containers/Sidebar/SidebarItem/__tests__/SidebarItem.unit.test.js
@@ -1,3 +1,23 @@
-import shallow from "enzyme";
+import { shallow } from "enzyme";
 import React from "react";
+
 import { SidebarItem } from "../SidebarItem.js";
+
+describe("SidebarItem", () => {
+  test("SidebarItem renders with no props passed in", () => {
+    const wrapper = shallow(<SidebarItem />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("highlighted SidebarItem renders with icon and label", () => {
+    const wrapper = shallow(
+      <SidebarItem highlighted icon="fire" label="Trending" />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("non-highlighted SidebarItem renders with icon and label", () => {
+    const wrapper = shallow(<SidebarItem icon="fire" label="Trending" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
