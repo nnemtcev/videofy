bd16077619490054d3af4569bcc48d3113718cb4
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-13 10:43:35 -0400

    Add snapshot test for HeaderNav component

---
+++ src/containers/HeaderNav/__tests__/HeaderNav.unit.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { HeaderNav } from "../HeaderNav.js";
+
+test("renders HeaderNav", () => {
+  const wrapper = shallow(<HeaderNav />);
+  expect(wrapper).toMatchSnapshot();
+});
