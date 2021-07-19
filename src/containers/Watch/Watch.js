fe06cb28d39011fec802799c94fdffaeccd12b02
Nikita Nemtcev <nnemtcev@uwo.ca>
2021-07-18 22:17:44 -0400

    Create basic layout for Watch component using CSS Grid

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -7,12 +7,28 @@ import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
 export class Watch extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <div style={{ maxWidth: "80%" }}>
-          <Video id="-7fuHEEmEjs" />
+      <div className="watch-grid">
+        <Video className="video" id="-7fuHEEmEjs" />
+        <div
+          className="metadata"
+          style={{ width: "100%", height: "100px", background: "#F91112" }}
+        >
+          Metadata
         </div>
-        <RelatedVideos />
-      </React.Fragment>
+        <div
+          className="video-info-box"
+          style={{ width: "100%", height: "100px", background: "#BD10E0" }}
+        >
+          Video Info Box
+        </div>
+        <div
+          className="comments"
+          style={{ width: "100%", height: "100px", background: "#9013FE" }}
+        >
+          Comments
+        </div>
+        <RelatedVideos className="relatedVideos" />
+      </div>
     );
   }
 }
